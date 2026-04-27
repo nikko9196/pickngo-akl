183921694215bf76322ae3edd626d13f8672c41a
Nikko Pham <nikko.090196@gmail.com>
2026-04-27 20:28:57 +1200

    Created and completed to implement the routes for vote, and mount it to the app.js. NOTE: Testing on POSTMAN hasn't been done yet + A note in the voteController.js needs to be considered + Also need to consider to add the GET request to fetch the vote summary.

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 const questionRoutes = require("./routes/questionRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
 const wheelRoutes = require("./routes/wheelRoutes");
+const voteRoutes = require("./routes/voteRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/questions", questionRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/sessions", wheelRoutes);
+app.use("/api/sessions", voteRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
