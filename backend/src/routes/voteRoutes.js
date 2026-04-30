8ea4e342c21308e4817445a38822461ab41e8873
Nikko Pham <nikko.090196@gmail.com>
2026-04-30 15:30:21 +1200

    Created and implemented getVoteSummary API to fetch vote counts without resolving vote.

--- backend/src/routes/voteRoutes.js
+++ backend/src/routes/voteRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { submitVote, resolveVote } = require("../controllers/voteController");
+const { submitVote, resolveVote, getVoteSummary } = require("../controllers/voteController");
 const { requireAuth } = require("../middleware/auth");
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.use(requireAuth);
 
 router.post("/:sessionId/vote", submitVote);
+router.get("/:sessionId/vote", getVoteSummary);
 router.post("/:sessionId/vote/result", resolveVote);
 
 module.exports = router;
